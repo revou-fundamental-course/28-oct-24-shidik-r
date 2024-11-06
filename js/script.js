ee6f06f534a3a171e540ffe1d68deb68eec70ba2
Shidik Rachmadhi <shidikrachmadhi08@gmail.com>
2024-11-07 02:38:18 +0700

    update html, css & js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 // ini js
+// Objek untuk menyimpan kategori BMI dan pesan terkaitnya
+const BMI_CATEGORIES = {
+  UNDERWEIGHT: "Kekurangan berat badan",
+  NORMAL: "Normal (ideal)",
+  OVERWEIGHT: "Kelebihan berat badan",
+  OBESITY: "Kegemukan (Obesitas)",
+};
